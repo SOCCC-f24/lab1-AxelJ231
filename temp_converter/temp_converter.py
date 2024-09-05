ca66a12cd355269ce4be9bfc97b1beaef0a8205c
AxelJ231 <adj16@sunyorange.edu>
2024-09-05 17:41:23 -0400

    Update temp_converter.py

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
+# Axel Jackson, 138, 9/5/24, CSC
+
 # process
 def c2f(c):
-    return c * 9 / 5 + 32
-
+    return c * (9 / 5) + 32
+# Takes celsius as an input, returns fahrenheit as an output
 def main(cel):
     return c2f(cel)
 
